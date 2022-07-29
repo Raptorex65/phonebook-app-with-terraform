c442a7a6e61a41c3289db14565262f5f532aea8e
Selcuk <71727239+Raptorex65@users.noreply.github.com>
2022-07-29 12:17:14 +0200

    Add files via upload

---
+++ tf-files/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.71.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "4.19.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "ec2user"
+    access_key = "AKIARDK6SEWPUVI2FQZW"
+    secret_key = "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"
+
+}
+
+provider "github" {
+  token = "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"
+}
+
