c924fbfef93bed828f754f70e54334163fa83c1d
Raptorex65 <71727239+Raptorex65@users.noreply.github.com>
2022-07-29 12:04:48 +0200

    terraform files updated

--- tf-files/provider.tf
+++ tf-files/provider.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 terraform {
   required_providers {
     aws = {
@@ -23,3 +24,30 @@ provider "github" {
   token = "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"
 }
 
+=======
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.71.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "4.19.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "ec2user"
+    access_key = "AKIARDK6SEWPUVI2FQZW"
+    secret_key = "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"
+
+}
+
+provider "github" {
+  token = "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"
+}
+
+>>>>>>> a08725d... terraform files updated
